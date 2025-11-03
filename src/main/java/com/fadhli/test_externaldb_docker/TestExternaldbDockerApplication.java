d8126058a369a7c153c1d2c53ec688b707adef59
fadhliazhar14 <fadhliazhar44@gmail.com>
2025-11-03 23:41:28 +0700

    Initial commit

---
+++ src/main/java/com/fadhli/test_externaldb_docker/TestExternaldbDockerApplication.java
@@ -0,0 +1,13 @@
+package com.fadhli.test_externaldb_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestExternaldbDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestExternaldbDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fadhli/test_externaldb_docker/TestExternaldbDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fadhli.test_externaldb_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestExternaldbDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
